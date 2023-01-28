3592e20295309235befa368d9289e70d264d3ff8
Ouvel Smata <ouvelsmata@gmail.com>
2023-01-28 02:41:21 +0000

    add basic route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.info("Hello there");
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response): void => {
+  res.send(`
+      <div>
+        <h1>Hello There!</h1>
+      </div>
+      `);
+});
+
+app.listen(3000, () => {
+  console.info("Server started on port 3000");
+});

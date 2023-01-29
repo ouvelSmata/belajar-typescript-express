f32dbd02a84143a02a4845f85d40341bdd6ffa4b
Ouvel Smata <ouvelsmata@gmail.com>
2023-01-29 03:13:23 +0000

    add parsing from body

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -6,4 +6,26 @@ router.get("/", (req: Request, res: Response) => {
   res.send("Hi There!");
 });
 
+router.get("/login", (req: Request, res: Response): void => {
+  res.send(`
+    <form method="POST">
+      <div>
+        <label for="username">Username</label>
+        <input type="text" name="username" />
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <input type="password" name="password" />
+      </div>
+        <button>Submit</submit>
+    </form>
+    `);
+});
+
+router.post("/login", (req: Request, res: Response): void => {
+  const { username, password } = req.body;
+
+  res.send(username + password);
+});
+
 export { router };

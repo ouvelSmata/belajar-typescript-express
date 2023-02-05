ae585d0e01fe82713ce2b6507bbaa2d332e5cf73
Ouvel Smata <ouvelsmata@gmail.com>
2023-02-05 00:54:53 +0000

    add protecting route

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,7 +1,17 @@
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
+
+  res.status(403);
+  res.send("Not Permitted");
 }
 
 const router = Router();
@@ -46,7 +56,7 @@ router.get("/", (req: Request, res: Response) => {
     res.send(`
       <div>
         <div>You are not login</div>
-        <a href="/login">Logout</a>
+        <a href="/login">Login</a>
       </div>
       `);
   }
@@ -55,6 +65,10 @@ router.get("/", (req: Request, res: Response) => {
 router.get("/logout", (req: Request, res: Response) => {
   req.session = undefined;
   res.redirect("/");
+});
+
+router.get("/protected", requireAuth, (req: Request, res: Response) => {
+  res.send("Welcome to protected routes, user logged in");
 });
 
 export { router };
